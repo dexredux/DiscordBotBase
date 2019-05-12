b7bb6265c096b6d1ad3f97d90d1278ca6495ea19
dexredux <45527015+dexredux@users.noreply.github.com>
2019-05-11 21:32:34 -0400

    uploaded bot
    This is bot without discord.NET packages

---
+++ botbase/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Rest;
+
+namespace botbase
+{
+    class Program
+    {
+        public DiscordSocketClient Client;
+        public CommandHandler Handler;
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+        public static string bottoken = "PUT YOUR BOT TOKEN HERE";
+
+
+        public async Task Start()
+        {
+            Client = new DiscordSocketClient();
+            Handler = new CommandHandler();
+
+            await Client.LoginAsync(Discord.TokenType.Bot, bottoken, true);
+            await Client.StartAsync();
+
+
+            await Handler.Install(Client);
+
+
+            // subscribe to Client.Ready callback
+            Client.Ready += Client_Ready;
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Ready()
+        {
+            Console.WriteLine("Were Online");
+
+            return;
+        }
+
+    }
+}
